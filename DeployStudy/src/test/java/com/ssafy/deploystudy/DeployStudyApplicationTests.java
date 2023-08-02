3cd854cbaea489816f7e1741b878112400fdcc6d
bigdog93 <ljc9393@nate.com>
2023-08-02 09:57:04 +0900

    first Test code

---
+++ DeployStudy/src/test/java/com/ssafy/deploystudy/DeployStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.deploystudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeployStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
